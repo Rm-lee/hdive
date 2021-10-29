640f1b6475af3e17380811c4d6229e65fdf37e57
roger-lee <rmlee0003@gmail.com>
2021-10-28 19:52:27 -0500

    navigation page setup, search bar animations created

--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import HidiveLogo from "../assets/HIDIVE_logo.svg";
 import "./nav.css";
 function Nav() {
+  const [searchExpand, setSearchExpand] = useState(false);
+  const closeSearch = () => {
+    const searchbar = document.querySelector("#searchbar");
+    const trialButton = document.querySelector("#trial-button");
+    if (searchExpand) {
+      setTimeout(() => {
+        searchbar.style.display = "none";
+        trialButton.style.display = "block";
+      }, 800);
+    } else {
+      searchbar.style.display = "block";
+      trialButton.style.display = "none";
+    }
+  };
+  useEffect(() => {
+    const searchbar = document.querySelector("#searchbar");
+    searchbar.style.display = "none";
+  }, []);
   return (
     <div className="nav">
       <div className="nav-container">
         <div className="logo-navlist">
           <ul className="nav-items">
-            <img className="hidive-logo" src={HidiveLogo} />
+            <img className="hidive-logo" src={HidiveLogo} alt="hidive logo" />
             <li>SIMULCASTS</li>
             <li>DUBS</li>
             <li>SERIES</li>
@@ -20,12 +38,35 @@ function Nav() {
             <li>HOME</li>
           </ul>
         </div>
+        <ul className="profile-container">
+          <li id="trial-button" className="trial-button">
+            START 14-DAY FREE TRIAL
+          </li>
+          <li>
+            <div
+              id="searchbar"
+              className={
+                searchExpand ? "searchbar expanded" : "searchbar retract"
+              }
+            >
+              <input placeholder="  Search Titles..." />{" "}
+            </div>
+          </li>
+          <li>
+            <FontAwesomeIcon
+              onClick={() => {
+                closeSearch();
+                setSearchExpand(!searchExpand);
+              }}
+              className="nav-icons"
+              icon="search"
+            />
+          </li>
+          <li>
+            <FontAwesomeIcon className="nav-icons" icon="user" />
+          </li>
+        </ul>
       </div>
-      <ul className="profile-container">
-        <li className="trial-button">START 14-DAY FREE TRIAL</li>
-        <li><FontAwesomeIcon className="nav-icons" icon="search" /></li>
-        <li><FontAwesomeIcon className="nav-icons" icon="user" /></li>
-      </ul>
     </div>
   );
 }
